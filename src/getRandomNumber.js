767098d7b3033f8b255e64bb8ce421d16e254448
qcomps <coby52522gmail.com>
2022-12-10 23:13:42 -0500

    adding a param for executor public key

--- src/getRandomNumber.js
+++ src/getRandomNumber.js
@@ -7,9 +7,8 @@ B62qpvpwLbLDTLQvA2EVBrX5QXmTQ7yy9442KhCj8R1zAk21LuVKtwd EKE5tDFXqosfM2RrPyNsoriF
 const executorPublicKeyStr = 'B62qmE7JDvUtHbEtGEGVrh74ZspvpZg1EWPR4CvNaC4Wp9iZC4UuE7h';
 const oraclePrivateKeyStr = 'EKE5tDFXqosfM2RrPyNsoriFaTHCRURqTW3Hw6PXcnUB9FRHGW1Z';
 
-function getRandomNumber() {
+function getRandomNumber(executorPublicKey=PublicKey.fromBase58(executorPublicKeyStr)) {
   const rand = Math.floor(Math.random() * 1_000_000);
-  const executorPublicKey = PublicKey.fromBase58(executorPublicKeyStr);
   const oraclePrivateKey = PrivateKey.fromBase58(oraclePrivateKeyStr);
 
   const encryption = Encryption.encrypt([Field(rand)], executorPublicKey);
